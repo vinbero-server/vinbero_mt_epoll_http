78427652bdf13333003db4b3170b04e4992dd62e
Byeonggon Lee <gonny952@gmail.com>
2016-08-01 10:56:11 +0900

    some changes

--- src/tcpcube_epoll_http.c
+++ src/tcpcube_epoll_http.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <tcpcube/tcpcube_module.h>
 #include <libgonc/gonc_list.h>
+#include "tcpcube_epoll_http.h"
 
 int tcpcube_epoll_module_init(struct tcpcube_module_args* module_args, struct tcpcube_module_list* module_list)
 {
@@ -11,10 +12,17 @@ int tcpcube_epoll_module_init(struct tcpcube_module_args* module_args, struct tc
     return 0;
 }
 
-int tcpcube_epoll_module_service(struct tcpcube_module* module, int* client_socket)
+int tcpcube_epoll_module_service(struct tcpcube_module* module, int* client_socket, char* client_buffer, size_t client_buffer_size)
 {
-    write(*client_socket, "HTTP/1.1 200 OK\r\nServer: tcpcube\r\nContent-Length: 5\r\nContent-Type: text/plain\r\n\r\nHELLO\r\n", sizeof("HTTP/1.1 200 OK\r\nServer: tcpcube\r\nContent-Length: 5\r\nContent-Type: text/plain\r\n\r\nHELLO\r\n"));
-    return 0;
+    ssize_t read_count;
+    while((read_count = read(*client_socket, client_buffer, client_buffer_size)) > 0)
+    {
+        write(STDOUT_FILENO, client_buffer, read_count);
+    }
+    if(read_count == -1)
+        return 1;
+    else if(read_count == 0)
+        return 0;
 }
 
 int tcpcube_epoll_module_destroy(struct tcpcube_module* module)
