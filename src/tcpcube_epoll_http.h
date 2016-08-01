a52eafcd3ca5204c5041103265194b0951c672e2
Byeonggon Lee <gonny952@gmail.com>
2016-08-01 11:05:32 +0900

    Revert "some changes"
    This reverts commit 78427652bdf13333003db4b3170b04e4992dd62e.

--- src/tcpcube_epoll_http.h
+++
@@ -1,8 +0,0 @@
-#ifndef _TCPCUBE_EPOLL_HTTP_H
-#define _TCPCUBE_EPOLL_HTTP_H
-
-int tcpcube_epoll_module_init(struct tcpcube_module_args* module_args, struct tcpcube_module_list* module_list);
-int tcpcube_epoll_module_service(struct tcpcube_module* module, int* client_socket, char* client_buffer, size_t client_buffer_size);
-int tcpcube_epoll_module_destroy(struct tcpcube_module* module);
-
-#endif
