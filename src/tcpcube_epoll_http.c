1cf1773640ab16e5805eac0f2a5ac473405ca404
Byeonggon Lee <gonny952@gmail.com>
2016-08-01 09:23:54 +0900

    Initial commit

---
+++ src/tcpcube_epoll_http.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <tcpcube/tcpcube_module.h>
+#include <libgonc/gonc_list.h>
+
+int tcpcube_epoll_module_init(struct tcpcube_module_args* module_args, struct tcpcube_module_list* module_list)
+{
+    struct tcpcube_module* module = malloc(sizeof(struct tcpcube_module));
+    GONC_LIST_ELEMENT_INIT(module);
+    GONC_LIST_APPEND(module_list, module);
+    return 0;
+}
+
+int tcpcube_epoll_module_service(struct tcpcube_module* module, int* client_socket)
+{
+    write(*client_socket, "HTTP/1.1 200 OK\r\nServer: tcpcube\r\nContent-Length: 5\r\nContent-Type: text/plain\r\n\r\nHELLO\r\n", sizeof("HTTP/1.1 200 OK\r\nServer: tcpcube\r\nContent-Length: 5\r\nContent-Type: text/plain\r\n\r\nHELLO\r\n"));
+    return 0;
+}
+
+int tcpcube_epoll_module_destroy(struct tcpcube_module* module)
+{
+    free(module);
+    return 0;
+}
