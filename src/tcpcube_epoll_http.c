e398449b6ab872cacaa8fe067bb58430755c1ddd
Byeonggon Lee <gonny952@gmail.com>
2016-08-02 14:45:39 +0900

    tcpcube_epoll_http_lexer

--- src/tcpcube_epoll_http.c
+++ src/tcpcube_epoll_http.c
@@ -12,16 +12,14 @@ int tcpcube_epoll_module_init(struct tcpcube_module_args* module_args, struct tc
     GONC_LIST_APPEND(module_list, module);
     return 0;
 }
-#include <stdio.h>
+
 int tcpcube_epoll_module_service(struct tcpcube_module* module, int* client_socket)
 {
     ssize_t buffer_size = 10;
     char* buffer = malloc(buffer_size);
     ssize_t read_size;
-    while((read_size = read(*client_socket, buffer, buffer_size - 1)) > 0)
+    while((read_size = read(*client_socket, buffer, buffer_size)) > 0)
     {
-        buffer[read_size] = '\0';
-        printf("READ(%d): (%s)\n", read_size, buffer);
     }
     if(read_size == -1)
     {
