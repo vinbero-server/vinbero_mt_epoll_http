8abcfcf95281576a55a26ce9f5b5c69d1f84b243
Byeonggon Lee <gonny952@gmail.com>
2016-08-03 10:51:52 +0900

    http_parser is not working well

--- src/tcpcube_epoll_http.c
+++ src/tcpcube_epoll_http.c
@@ -22,12 +22,16 @@
 
 int on_header_field_callback(http_parser* parser, const char* at, size_t length)
 {
+    warnx("on_header_field_callback");
     write(STDOUT_FILENO, at, length);
+    write(STDOUT_FILENO, "\n", 1);
 }
 
 int on_header_value_callback(http_parser* parser, const char* at, size_t length)
 {
+    warnx("on_header_value_callback");
     write(STDOUT_FILENO, at, length);
+    write(STDOUT_FILENO, "\n", 1);
 }
 
 int tcpcube_epoll_module_init(struct tcpcube_module_args* module_args, struct tcpcube_module_list* module_list)
@@ -40,13 +44,31 @@ int tcpcube_epoll_module_init(struct tcpcube_module_args* module_args, struct tc
 
 int tcpcube_epoll_module_service(struct tcpcube_module* module, struct tcpcube_epoll_data* client_data)
 {
-    http_parser parser;
-    ssize_t buffer_size = 10;
-    char* buffer = malloc(buffer_size);
-    ssize_t read_size;
-    while((read_size = read(client_data->fd, buffer, buffer_size)) > 0)
+    if(client_data->ptr == 0)
     {
-        warnx("%s", buffer);
+        warnx("client_data->ptr not initialized");
+        client_data->ptr = malloc(sizeof(struct tcpcube_epoll_http_client_data));
+        ((struct tcpcube_epoll_http_client_data*)client_data->ptr)->http_parser = malloc(sizeof(struct http_parser));
+        http_parser_init(((struct tcpcube_epoll_http_client_data*)client_data->ptr)->http_parser, HTTP_REQUEST);
+        ((struct tcpcube_epoll_http_client_data*)client_data->ptr)->http_parser->data = &client_data->fd; // why is this needed?
+        ((struct tcpcube_epoll_http_client_data*)client_data->ptr)->http_parser_settings = malloc(sizeof(struct http_parser_settings));
+        ((struct tcpcube_epoll_http_client_data*)client_data->ptr)->http_parser_settings->on_header_field = on_header_field_callback;
+        ((struct tcpcube_epoll_http_client_data*)client_data->ptr)->http_parser_settings->on_header_value = on_header_value_callback;
+        ((struct tcpcube_epoll_http_client_data*)client_data->ptr)->http_buffer = malloc(1024 * sizeof(char));
+        ((struct tcpcube_epoll_http_client_data*)client_data->ptr)->http_buffer_size = 1024;
+    }
+    ssize_t read_size;
+    int parsed_size;
+    while((read_size = read(client_data->fd, ((struct tcpcube_epoll_http_client_data*)client_data->ptr)->http_buffer, ((struct tcpcube_epoll_http_client_data*)client_data->ptr)->http_buffer_size)) > 0)
+    {
+        warnx("read chars: %d", read_size);
+        while((parsed_size = http_parser_execute(((struct tcpcube_epoll_http_client_data*)client_data->ptr)->http_parser,
+             ((struct tcpcube_epoll_http_client_data*)client_data->ptr)->http_parser_settings,
+             ((struct tcpcube_epoll_http_client_data*)client_data->ptr)->http_buffer,
+             read_size)) > 0)
+        {
+            warnx("http_parser_execute(), parsed_size: %d", parsed_size);
+        }
     }
     if(read_size == -1)
     {
@@ -64,6 +86,11 @@ int tcpcube_epoll_module_service(struct tcpcube_module* module, struct tcpcube_e
     else if(read_size == 0)
     {
         warnx("%s: %u: client socket EOF", __FILE__, __LINE__);
+        free(((struct tcpcube_epoll_http_client_data*)client_data->ptr)->http_parser);
+        free(((struct tcpcube_epoll_http_client_data*)client_data->ptr)->http_parser_settings);
+        free(((struct tcpcube_epoll_http_client_data*)client_data->ptr)->http_buffer);
+        free(client_data->ptr);
+        client_data->ptr = NULL;
         return 0;
     }
 }
--- src/tcpcube_epoll_http.h
+++ src/tcpcube_epoll_http.h
@@ -1,12 +1,20 @@
 #ifndef _TCPCUBE_EPOLL_HTTP_H
 #define _TCPCUBE_EPOLL_HTTP_H
 
-#include <pthread.h>
+#include <http_parser.h>
 
 struct tcpcube_epoll_data
 {
     int fd;
-    void* custom_data;
+    void* ptr;
+};
+
+struct tcpcube_epoll_http_client_data
+{
+    struct http_parser* http_parser;
+    struct http_parser_settings* http_parser_settings;
+    char* http_buffer;
+    ssize_t http_buffer_size;
 };
 
 int tcpcube_epoll_module_init(struct tcpcube_module_args* module_args, struct tcpcube_module_list* module_list);
